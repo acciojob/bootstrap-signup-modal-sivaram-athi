83ccd0d8e21fa9f504b5e2e122b646ff3e1c961d
sivaram-athi <137918320+sivaram-athi@users.noreply.github.com>
2024-06-25 16:41:17 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your JS code here. If required.
-cy.get('button').first().click().then(() => {
-  cy.get('.modal').should('be.visible');
-  cy.get('.btn-close').click().then(() => {
-    cy.get('.modal').should('not.be.visible');
-  });
-});
